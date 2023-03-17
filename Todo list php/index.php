709266a86e6fb7c5e7a57aab65afc966599e9445
benjaminadx <andrieux.benjamin1@gmail.com>
2023-03-17 10:55:19 +0100

    mise en ligne sur always data

--- Todo list php/index.php
+++ Todo list php/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$pdo = new PDO("mysql:host=localhost;dbname=todolist", "root", "root");
+$pdo = new PDO("mysql:host=mysql-andrieux.alwaysdata.net;dbname=andrieux_todolist", "andrieux", "Camels01*");
 
 if (isset($_POST)) {
 
